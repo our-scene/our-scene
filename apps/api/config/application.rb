5728d431af01e8c22a7c99837730c00777fa85fa
bryanhclark <bryanhclark@gmail.com>
2022-11-12 16:22:46 -0500

    pls

--- apps/api/config/application.rb
+++ apps/api/config/application.rb
@@ -22,7 +22,6 @@ module Api
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.assets.enabled = false
     config.api_only = true
   end
 end
---
+++ apps/api/lib/tasks/railway.rake
@@ -0,0 +1,6 @@
+
+namespace :assets do
+  task 'precompile' do
+    puts "Not precompiling assets..."
+  end
+end
