c26e9fe30491d9104109bb5015d00278e12114df
bryanhclark <bryanhclark@gmail.com>
2022-11-14 15:48:07 -0500

    disable welcome controller

--- apps/api/app/controllers/application_controller.rb
+++ apps/api/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def not_found
+    render plain: 'These are not the droids you are looking for', status: 404
+  end
 end
--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'application#not_found'
 end
