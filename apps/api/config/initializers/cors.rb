58d422259109c28956e7f0628a5dc95d23e74119
bryanhclark <bryanhclark@gmail.com>
2022-11-16 14:07:37 -0500

    fix cors

--- apps/api/config/initializers/cors.rb
+++ apps/api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5000'
+    origins ENV['OUR_SCENE_CLIENT_DOMAIN']
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
