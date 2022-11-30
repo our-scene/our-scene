20f3c82d0761ec7e30501cabb27f2cc3da74df58
kroy94 <kelseyroy@mac.com>
2022-11-30 10:39:32 -0800

    OUR-30: tailwind is working effectively, added google fonts and next font library

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Exo } from "@next/font/google";
+import 'tailwindcss/tailwind.css';
+
+// If loading a variable font, you don't need to specify the font weight
+const exo = Exo({
+  weight: "900",
+  // style: 'black',
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={exo.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,17 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css"; 
+// import 'tailwindcss/tailwind.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col justify-items-center items-center">
       <Head>
         <title>Our Scene Coming Soon</title>
         <meta name="description" content="Our Scene coming soon page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>This shit works</main>
+      <main>This shit works</main>
     </div>
   );
 }
