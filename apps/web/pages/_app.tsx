244a4dca7ebbca5cc9652e07dee8a13f78efbd97
kroy94 <kelseyroy@mac.com>
2022-11-30 12:20:15 -0800

    OUR-30: fixed height of main class, built basic ComingSoon component, added various styling

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -5,15 +5,26 @@ import "tailwindcss/tailwind.css";
 
 // If loading a variable font, you don't need to specify the font weight
 const exo = Exo({
-  weight: "900",
+  // weight: "900",
   // style: 'black',
   subsets: ["latin"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={exo.className}>
+    // <main className={exo.className}>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${exo.style.fontFamily};
+          height: 100%;
+        }
+        .main {
+          min-height: 100vh;
+        }
+      `}</style>
       <Component {...pageProps} />
-    </main>
+      {/* </main> */}
+    </>
   );
 }
