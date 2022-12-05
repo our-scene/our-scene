1760df2ec5c33c6546faea7b102f2016a1e894e1
kroy94 <kelseytylerroy@gmail.com>
2022-12-05 12:04:56 -0800

    work in progress

--- apps/web/pages/events/index.tsx
+++ apps/web/pages/events/index.tsx
@@ -1,10 +1,24 @@
 import { EventsForm } from "../../components/Forms/EventsForm";
 import { EventsFormValues } from "../../components/Forms/EventsForm";
+const events = require('../api/fakeData/indexEvents.json')
+
+interface EventsMapValues {
+  id: number,
+  title: string,
+  summary: string,
+  location: string,
+  description: string,
+  start: string,
+  end: string
+}
 
 export default function Events() {
   const handleAddEventSubmit = (values: EventsFormValues) => {
     console.log(values)
-    // to use react query to post to db eventually. 
+    // to use react query to post to db eventually.
+    // const event = (values: EventsMapValues)
+    const id = events.length ? Math.max(...events.map((event: EventsMapValues) => event.id)) + 1 : 1;
+    
   }
   return (
     <div>
