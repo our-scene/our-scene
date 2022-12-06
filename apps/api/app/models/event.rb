3317b3f556929b5329d21789665f1de12df64649
bryanhclark <bryanhclark@gmail.com>
2022-12-05 22:06:37 -0500

    OUR-42: rails generate resource Event

---
+++ apps/api/app/models/event.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Event < ApplicationRecord
+end
