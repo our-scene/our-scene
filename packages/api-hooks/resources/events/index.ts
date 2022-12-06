9dabf2b171e84b0643e6a8cef16c6fc1cc4290a0
bryanhclark <bryanhclark@gmail.com>
2022-12-05 22:07:15 -0500

    OUR-42: create useGetAllEventsQuery

--- packages/api-hooks/resources/events/index.ts
+++ packages/api-hooks/resources/events/index.ts
@@ -1,18 +1,22 @@
 import axios from 'axios'
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, UseQueryOptions } from "@tanstack/react-query"
 import { GetAllEvents } from './types'
+import { createAxiosClientWithAuth } from '../../lib/axios';
 
 const EVENTS_ROOT_PATH = '/events';
 
 
-const getAllEvents = async () => {
-  const { data } = await axios.get<GetAllEvents.Response>(EVENTS_ROOT_PATH);
-  return data
+const generateGetAllEventsWithAuth = (idToken: string) => {
+  console.log({ idToken })
+  const client = createAxiosClientWithAuth(idToken);
+  const fn = async () => {
+    const { data } = await client.get<GetAllEvents.Response>(EVENTS_ROOT_PATH);
+    return data;
+  };
+  return { path: EVENTS_ROOT_PATH, fn };
 }
 
-export const useGetAllEventsQuery = () => {
-  return useQuery<GetAllEvents.Response>(
-    [EVENTS_ROOT_PATH],
-    getAllEvents
-  )
+export const useGetAllEventsQuery = (sessionIdToken: string, options: UseQueryOptions<GetAllEvents.Response> = {}) => {
+  const { path: queryKey, fn } = generateGetAllEventsWithAuth(sessionIdToken);
+  return useQuery<GetAllEvents.Response>([queryKey], fn, options);
 }
