db04608734b81253f08fb3781ccd3530b7be19ed
bryanhclark <bryanhclark@gmail.com>
2022-12-19 18:29:15 -0500

    OS-49: stub create events / places pages

--- apps/web/pages/places/index.tsx
+++ apps/web/pages/places/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { BaseLayout } from '../../components/layout/BaseLayout';
 
 export default function Places() {
@@ -5,7 +6,9 @@ export default function Places() {
     <BaseLayout>
       <div className="flex w-full justify-center items-center">
         <div className="flex text-primary">
-          <div className="text-primary">Places</div>
+          <Link href="/places/new">
+            <div className="btn bg-primary text-primary-content">Add Place</div>
+          </Link>
         </div>
       </div>
     </BaseLayout>
