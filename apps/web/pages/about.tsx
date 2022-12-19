a54179dca9d62c2b81b77147ee28e2141977c380
bryanhclark <bryanhclark@gmail.com>
2022-12-19 18:18:58 -0500

    OS-49: stub pages

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -3,7 +3,11 @@ import { BaseLayout } from '../components/layout/BaseLayout';
 export default function About() {
   return (
     <BaseLayout>
-      <div>About us</div>
+      <div className="flex w-full justify-center items-center">
+        <div className="flex text-primary">
+          <div className="text-primary">About</div>
+        </div>
+      </div>
     </BaseLayout>
   );
 }
