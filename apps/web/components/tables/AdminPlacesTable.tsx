c2d4a4f6c9b32485e4f88550e53b0f4dffb397a8
bryanhclark <bryanhclark@gmail.com>
2023-01-03 16:24:05 -0500

    OS-46: admin places table && form updates

--- apps/web/components/tables/AdminPlacesTable.tsx
+++ apps/web/components/tables/AdminPlacesTable.tsx
@@ -2,32 +2,29 @@ import { Place } from '@our-scene/api-hooks/resources/admin/places';
 
 export const AdminPlacesTable = ({ places }: { places: Place[] }) => {
   return (
-    <div>
-      Admin Places table
-      <div className="overflow-x-auto">
-        <table className="table table-zebra w-full">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Status</th>
-              <th>Created By</th>
-              <th>Created At</th>
+    <div className="overflow-x-auto">
+      <table className="table table-normal w-full">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Status</th>
+            <th>Created By</th>
+            <th>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {places.map((place) => (
+            <tr className="hover" key={place.id}>
+              <td>{place.id}</td>
+              <td>{place.title}</td>
+              <td>{place.status}</td>
+              <td>{place.user.name}</td>
+              <td>{new Date(place.createdAt).toDateString()}</td>
             </tr>
-          </thead>
-          <tbody>
-            {places.map((place) => (
-              <tr key={place.id}>
-                <td>{place.id}</td>
-                <td>{place.title}</td>
-                <td>{place.status}</td>
-                <td>{place.user.name}</td>
-                <td>{place.createdAt}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
