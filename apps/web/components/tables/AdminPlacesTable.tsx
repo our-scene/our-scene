4491f0772f466889f3b06b4d573642da951ad9a3
bryanhclark <bryanhclark@gmail.com>
2023-01-03 17:19:03 -0500

    OS-46: AdminPlacesTable good enough for now

--- apps/web/components/tables/AdminPlacesTable.tsx
+++ apps/web/components/tables/AdminPlacesTable.tsx
@@ -1,26 +1,35 @@
 import { Place } from '@our-scene/api-hooks/resources/admin/places';
+import Link from 'next/link';
 
 export const AdminPlacesTable = ({ places }: { places: Place[] }) => {
+  // TODO: figure out how to set this at the theme level
+  const thClass = 'bg-primary';
   return (
     <div className="overflow-x-auto">
       <table className="table table-normal w-full">
         <thead>
           <tr>
-            <th>Id</th>
-            <th>Title</th>
-            <th>Status</th>
-            <th>Created By</th>
-            <th>Created At</th>
+            <th className={thClass}>Id</th>
+            <th className={thClass}>Title</th>
+            <th className={thClass}>Status</th>
+            <th className={thClass}>Created By</th>
+            <th className={thClass}>Created At</th>
+            <th className={thClass}></th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="text-primary">
           {places.map((place) => (
-            <tr className="hover" key={place.id}>
+            <tr key={place.id}>
               <td>{place.id}</td>
               <td>{place.title}</td>
               <td>{place.status}</td>
               <td>{place.user.name}</td>
               <td>{new Date(place.createdAt).toDateString()}</td>
+              <td>
+                <Link href={`/admin/places/${place.id}`}>
+                  <button className="btn btn-primary btn-xs">details</button>
+                </Link>
+              </td>
             </tr>
           ))}
         </tbody>
