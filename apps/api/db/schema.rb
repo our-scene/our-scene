964abdd3e37db46a4603ee14929b5ddf83493bb1
bryanhclark <bryanhclark@gmail.com>
2023-01-03 12:46:04 -0500

    OS-46: add status enum to places model

--- apps/api/db/schema.rb
+++ apps/api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_213620) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_03_164339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_213620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "status"
   end
 
   create_table "users", force: :cascade do |t|
