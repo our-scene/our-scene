3fe800f78632996985fb0e01e04ef912dc0b12ed
bryanhclark <bryanhclark@gmail.com>
2023-01-11 22:14:17 -0500

    OUR-54: pull data on place details view

--- apps/web/pages/admin/places/[id].tsx
+++ apps/web/pages/admin/places/[id].tsx
@@ -1,12 +1,21 @@
+import { useAdminGetPlaceQuery } from '@our-scene/api-hooks/resources/admin/places';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { AdminLayout } from '../../../components/layout/AdminLayout';
 
 const AdminPlaceDetails = () => {
   const router = useRouter();
-  const { id } = router.query;
+  const { id: placeId } = router.query;
+  const { data: session } = useSession();
+  const { data, isLoading, isError } = useAdminGetPlaceQuery(
+    session?.idToken as string,
+    { id: placeId as string },
+    { enabled: Boolean(placeId) }
+  );
+
   return (
     <AdminLayout>
-      <div>AdminPlaceDetails: {id}</div>
+      <div>AdminPlaceDetails: {placeId}</div>
     </AdminLayout>
   );
 };
