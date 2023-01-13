4982532f40323b5170e420949a325fdbd3bc9009
bryanhclark <bryanhclark@gmail.com>
2023-01-13 15:57:49 -0500

    OUR-54: stub image component

--- apps/web/components/lib/inputs/ImageInput.tsx
+++ apps/web/components/lib/inputs/ImageInput.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
-import React, { HTMLInputTypeAttribute, useState } from 'react';
+import React, { useState } from 'react';
 
-export const ImageInput = () => {
+export const ImageUploadInput = () => {
   const [image, setImage] = useState(null);
   const [createObjectURL, setCreateObjectURL] = useState('');
 
   const handleInputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e);
+    console.log('e: ', e);
+    const file = e.currentTarget.files;
+    console.log({ file });
   };
 
   return (
